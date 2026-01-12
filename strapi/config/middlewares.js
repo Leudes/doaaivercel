b1d09378e47c48d5547bf80fe002479ece95f218
Leudes <108896134+Leudes@users.noreply.github.com>
2026-01-12 06:45:59 -0300

    fixing CORS

--- strapi/config/middlewares.js
+++ strapi/config/middlewares.js
@@ -9,7 +9,7 @@ module.exports = [
     config: {
       enabled: true,
       headers: '*',
-      origin: ['http://localhost:5173', 'http://localhost:1337'], // Adicione as URLs do seu front e back
+      origin: ['http://localhost:5173', 'http://localhost:1337', 'https://doaaivercel.vercel.app'], // Adicione as URLs do seu front e back
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
     },
   },
